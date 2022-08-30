e700f768be9412e4301153a514fa9b84fd5fc8af
iuryliberato <iuryliberato@hotmail.com>
2022-08-30 20:38:01 +0100

    added load more button and a loading spinner

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import logo from "./images/logo.png"
+import spinner from "./images/spinner1.gif"
+
 
 const App = () => {
 
-  const [articleNews, setArticleNews] = useState({ articles: [] })
+  const [articleNews, setArticleNews] = useState([])
   const [activeArticle, setActiveArticle] = useState()
   const [search, setSearch] = useState('')
   const [url, setUrl] = useState('all')
+  const [error, setError] = useState(false)
+
+  const [pageNumber, setPageNumber] = useState(1)
+
 
   useEffect(() => {
     const getData = async () => {
-      const { data } = await axios.get(`https://free-news.p.rapidapi.com/v1/search?q=${url}`,
-        {
-          headers: {
-            'X-RapidAPI-Key': '09842dab04mshe31fa8e370e59f8p1e602ajsn6ac665e52cc0',
-            'X-RapidAPI-Host': 'free-news.p.rapidapi.com'
-          }
-        }) // * <-- replace with your endpoint
-      console.log('e', url)
-      setArticleNews(data)
+      try {
+        const { data } = await axios.get(`https://free-news.p.rapidapi.com/v1/search?q=${url}&page=${pageNumber}`,
+          {
+            headers: {
+              'X-RapidAPI-Key': '09842dab04mshe31fa8e370e59f8p1e602ajsn6ac665e52cc0',
+              'X-RapidAPI-Host': 'free-news.p.rapidapi.com'
+            }
+          }) // * <-- replace with your endpoint
+        console.log('e', url)
+        setArticleNews(currentArticles => currentArticles.concat(data.articles))
+
+        console.log(data.page)
+      } catch (error) {
+        setError(true)
+      }
+
     }
     getData()
-  }, [url])
+  }, [url, pageNumber])
 
+  useEffect(() => {
+    setPageNumber(1)
+    setArticleNews([])
+  }, [url])
 
 
   useEffect(() => {
@@ -42,7 +59,9 @@ const App = () => {
     setUrl(e.target.value)
   }
 
+
   return (
+
     <>
       <header>
         <img className="logo" src={logo} alt="logo" />
@@ -52,7 +71,7 @@ const App = () => {
           <input type="search" placeholder="Search News 🔎" onChange={onSearch}></input>
         </div>
         <select onChange={handleUrl}>
-          <option value="all">All</option>
+          <option value="all">All News</option>
           <option value="Sports">Sports</option>
           <option value="tv">TV</option>
           <option value="weather">Weather</option>
@@ -63,49 +82,70 @@ const App = () => {
           <option value="culinary">Culinary</option>
         </select>
       </nav>
-
-      {
-        articleNews.articles.filter(news => (
-          news.title.toLowerCase().includes(search.toLowerCase())
-        )).map(news => (
-          <div className="container">
-            <div key={news._id} className="card">
-              <img className="images-card" src={news.media} alt={news.title} />
-              <div className="description">
-                <h2>{news.title}</h2>
-                <p>{news.published_date}</p>
-                <button onClick={() => setActiveArticle(news)}>View more</button>
-              </div>
-            </div>
-          </div>
-        ))
-      }
-      {
-        activeArticle && (
-          <div>
-            <div className="popUp active">
-              <div className="popUpInner">
-                <div>
-                  <img className="image" src={activeArticle.media} alt={activeArticle.title} />
-                  <h2>{activeArticle.title}</h2>
-                  <p>{activeArticle.topic}</p>
-                  <p>{activeArticle.published_date}</p>
-                </div>
-                <p className="summary">{activeArticle.summary}</p>
-
-                <p className="summary">Rights: {activeArticle.rights}</p>
-
-                <div className="buttons">
-                  <a className="close" href={activeArticle.link} target="_blank" rel="noreferrer">View full Article</a>
-                  <button className="close" onClick={() => setActiveArticle(null)}>Close</button>
+      {articleNews.length ? (
+        <>
+          {
+            articleNews.filter(news => (
+              news.title.toLowerCase().includes(search.toLowerCase())
+            )).map(news => (
+              <div className="container">
+                <div key={news._id} className="card">
+                  <img className="images-card" src={news.media} alt={news.title} />
+                  <div className="description">
+                    <h2>{news.title}</h2>
+                    <p>{news.published_date}</p>
+                    <button onClick={() => setActiveArticle(news)}>View more</button>
+                  </div>
                 </div>
               </div>
-            </div>
+            ))
+          }
+          <div className="load-more-bt">
+            <button onClick={() => setPageNumber(pageNumber + 1)} className="load-more">Load more</button>
           </div>
-        )
+          {
+            activeArticle && (
+              <div>
+                <div className="popUp active">
+                  <div className="popUpInner">
+                    <div>
+                      <img className="image" src={activeArticle.media} alt={activeArticle.title} />
+                      <h2>{activeArticle.title}</h2>
+                      <p>{activeArticle.topic}</p>
+                      <p>{activeArticle.published_date}</p>
+                    </div>
+                    <p className="summary">{activeArticle.summary}</p>
+
+                    <p className="summary">Rights: {activeArticle.rights}</p>
+
+                    <div className="buttons">
+                      <a className="close" href={activeArticle.link} target="_blank" rel="noreferrer">View full Article</a>
+                      <button className="close" onClick={() => setActiveArticle(null)}>Close</button>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            )
+          }
+        </>
+      )
+        :
+        <>
+          {error ?
+            <h3>Oops something went wrong.</h3>
+            :
+            <div className="load-more-bt">
+              <img src={spinner} alt={spinner} />
+            </div>
+          }
+        </>
       }
+
     </>
+
   )
+
 }
 
 export default App
